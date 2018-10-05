8f8df5b637fc7061c6bb64e03380861d13dc0b8c
GarveyMin <mjw5201314@foxmail.com>
2018-10-05 22:47:07 +0800

    synch Oct 5th

--- src/com/javaCore/test/FindDirectories.java
+++ src/com/javaCore/test/FindDirectories.java
@@ -6,13 +6,16 @@ public class FindDirectories {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// if no arguments provided, start at the parent directory
 		if (args.length == 0)
 			args = new String[] { ".." };
 		try {
 			File pathName = new File(args[0]);
 			String[] fileNames = pathName.list();
 			for (int i = 0; i < fileNames.length; i++) {
+				// enumerate all files in the directory
 				File f = new File(pathName.getPath(), fileNames[i]);
+				// if the file is again a directory, call the main method recursively
 				if (f.isDirectory()) {
 					System.out.println(f.getCanonicalPath());
 					main(new String[] { f.getPath() });
